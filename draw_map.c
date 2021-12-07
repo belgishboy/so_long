a3fe0ec860ae8e64d7e8cbfcfdc345850c4a79ae
Vincent Heymans <vheymans@c2s17d101.42wolfsburg.de>
2021-12-07 18:58:30 +0100

    maggi valgrind

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: vheymans <vheymans@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 18:13:14 by vheymans          #+#    #+#             */
-/*   Updated: 2021/12/06 15:18:58 by vheymans         ###   ########.fr       */
+/*   Updated: 2021/12/06 15:46:50 by vheymans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,6 @@ int	draw_map(t_game *g)
 		}
 		row ++;
 	}
-	mlx_string_put(g->mlx, g->mlx_win, 2, 2, 111, ft_itoa(g->count));// find a valid color
+	mlx_string_put(g->mlx, g->mlx_win, 2, 2, 111, ft_itoa(g->move));// find a valid color
 	return (1);
 }
